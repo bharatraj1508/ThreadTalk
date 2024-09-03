de5567f6ff079039060082c8b55c35e9d3ec1ebe
Bharat Raj Verma <bharat.raj1508@gmail.com>
2024-09-03 19:51:35 -0400

    confirm_password issue resolved

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -3,13 +3,14 @@ import { User } from '../../core/interfaces/user';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../../core/services/authentication/auth.service';
 import { ViewChild } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
-export class RegisterComponent {
+export class RegisterComponent implements AfterViewInit {
   newUser: User = { first_name: '', last_name: '', email: '', password: '' };
   @ViewChild('signUpForm') signUpForm!: NgForm;
   formError: boolean = false;
@@ -21,7 +22,10 @@ export class RegisterComponent {
   constructor(public authService: AuthService) {}
 
   onSubmit(signUpForm: NgForm) {
-    if (signUpForm.invalid) {
+    if (
+      signUpForm.invalid ||
+      signUpForm.value.password !== signUpForm.value.confirm_password
+    ) {
       this.formError = true;
       this.message =
         'Please review and correct the highlighted fields before submitting.';
@@ -35,6 +39,13 @@ export class RegisterComponent {
         this.success = true;
         this.message =
           'Your account has been created successfully. Please login with your credentials';
+        this.signUpForm.resetForm();
+        this.newUser = {
+          first_name: '',
+          last_name: '',
+          email: '',
+          password: '',
+        };
       },
       (error) => {
         if (error.status == 409) {
@@ -44,6 +55,31 @@ export class RegisterComponent {
         }
       }
     );
+  }
+
+  ngAfterViewInit() {
+    this.signUpForm.form.valueChanges.subscribe(() => {
+      this.checkFormValidity();
+      // Reset form controls to untouched state
+      Object.keys(this.signUpForm.form.controls).forEach((key) => {
+        const control = this.signUpForm.form.get(key);
+        if (control) {
+          control.markAsUntouched();
+        }
+      });
+    });
+  }
+
+  checkFormValidity() {
+    if (
+      this.signUpForm.form.valid &&
+      this.signUpForm.value.password === this.signUpForm.value.confirm_password
+    ) {
+      this.formError = false;
+      this.warning = false;
+      this.success = false;
+      this.message = '';
+    }
   }
 
   //this funciton will visit all the controls in the form, to check the invalid controls
