95ee1b9fc98fc7a16eadafa0dc3da29d3b0fe1bd
Bharat Raj Verma <bharat.raj1508@gmail.com>
2024-09-10 11:21:59 -0400

    desigined dashboard, sidebars, signout, and implemented auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth/auth.guard';
 
 const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'd',
+    loadChildren: () =>
+      import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+    canActivate: [authGuard],
   },
 ];
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,24 @@ import { CommonModule } from '@angular/common';
 import { ErrorComponent } from './prompts/error/error.component';
 import { WarningComponent } from './prompts/warning/warning.component';
 import { SuccessComponent } from './prompts/success/success.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { SidebarRightComponent } from './sidebar-right/sidebar-right.component';
 
 @NgModule({
-  declarations: [ErrorComponent, WarningComponent, SuccessComponent],
+  declarations: [
+    ErrorComponent,
+    WarningComponent,
+    SuccessComponent,
+    SidebarComponent,
+    SidebarRightComponent,
+  ],
   imports: [CommonModule],
-  exports: [ErrorComponent, WarningComponent, SuccessComponent],
+  exports: [
+    ErrorComponent,
+    WarningComponent,
+    SuccessComponent,
+    SidebarComponent,
+    SidebarRightComponent,
+  ],
 })
 export class SharedModule {}
